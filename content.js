5de21e3be3b111de78e138b6d94ddd5345b2d99c
Sihal3 <53540271+Sihal3@users.noreply.github.com>
2021-08-08 23:15:05 -0700

    only replaces sanjana to sanju now

--- content.js
+++ content.js
@@ -1,27 +1,21 @@
 var elements = document.getElementsByTagName('*');
-var fromword = [];
-var toword = [];
+var fromword = "Sanjana";
+var toword = "Sanju";
 
-chrome.storage.sync.get(['fromword', 'toword'], function(results){
-	fromword = results.fromword;
-	toword = results.toword;
-	for (var k = 0; k < fromword.length; k++){
-		for (var i = 0; i < elements.length; i++) {
-		var element = elements[i];
-			for (var j = 0; j < element.childNodes.length; j++) {
-				var node = element.childNodes[j];
-				
-					if (node.nodeType === 3) {
-						var text = node.nodeValue;
-						var reggy = new RegExp(fromword[k], 'g'+'i');
-						var replacedText = text.replace(reggy, toword[k]);
-						if (replacedText !== text) {
-							element.replaceChild(document.createTextNode(replacedText), node);
-						}
-					}
+for (var i = 0; i < elements.length; i++) {
+var element = elements[i];
+	for (var j = 0; j < element.childNodes.length; j++) {
+		var node = element.childNodes[j];
+
+			if (node.nodeType === 3) {
+				var text = node.nodeValue;
+				var reggy = new RegExp(fromword, 'g'+'i');
+				var replacedText = text.replace(reggy, toword);
+				if (replacedText !== text) {
+					element.replaceChild(document.createTextNode(replacedText), node);
+				}
 			}
-		}
 	}
-	
-});
+}
+
 

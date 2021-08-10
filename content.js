936f408cb697c82f566b90c0e10e29c49a4a1e56
Sihal3 <53540271+Sihal3@users.noreply.github.com>
2021-08-10 16:27:56 -0700

    update to allow multiple names

--- content.js
+++ content.js
@@ -1,20 +1,24 @@
-var elements = document.getElementsByTagName('*');
-var fromword = "Sanjana";
-var toword = "Sanju";
+if(document.body.contains(document.getElementById('wpadminbar'))){
+	var elements = document.getElementsByTagName('*');
+	var fromword = ["Sanjana"];
+	var toword = ["Sanju"];
 
-for (var i = 0; i < elements.length; i++) {
-var element = elements[i];
-	for (var j = 0; j < element.childNodes.length; j++) {
-		var node = element.childNodes[j];
-
-			if (node.nodeType === 3) {
-				var text = node.nodeValue;
-				var reggy = new RegExp(fromword, 'g'+'i');
-				var replacedText = text.replace(reggy, toword);
-				if (replacedText !== text) {
-					element.replaceChild(document.createTextNode(replacedText), node);
+	for (var k = 0; k < fromword.length; k++){
+		for (var i = 0; i < elements.length; i++) {
+		var element = elements[i];
+			for (var j = 0; j < element.childNodes.length; j++) {
+				var node = element.childNodes[j];
+				
+				if (node.nodeType === 3) {
+					var text = node.nodeValue;
+					var reggy = new RegExp(fromword[k], 'g'+'i');
+					var replacedText = text.replace(reggy, toword[k]);
+					if (replacedText !== text) {
+						element.replaceChild(document.createTextNode(replacedText), node);
+					}
 				}
 			}
+		}
 	}
 }
 
